2be9f165c887a1b06020c980ef40beaa96a7013b
Асель Абдрахманова <dellya4>
2026-05-23 01:16:27 +0500

    feat: add password hashing

--- src/main/java/org/example/sdf_final/service/impl/AuthServiceImpl.java
+++ src/main/java/org/example/sdf_final/service/impl/AuthServiceImpl.java
@@ -6,14 +6,22 @@ import org.example.sdf_final.dto.response.AuthResponse;
 import org.example.sdf_final.entity.User;
 import org.example.sdf_final.repository.UserRepository;
 import org.example.sdf_final.service.AuthService;
+import org.example.sdf_final.security.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public AuthResponse register(RegisterRequest request) {
@@ -21,12 +29,15 @@ public class AuthServiceImpl implements AuthService {
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .role(request.getRole())
                 .build();
 
         userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user.getEmail());
 
         return AuthResponse.builder()
+                .token(jwtToken)
                 .email(user.getEmail())
                 .role(user.getRole().name())
                 .build();
@@ -34,13 +45,21 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public AuthResponse login(AuthRequest request) {
-        //Stabdart manager Spring Security
+        //Standard manager Spring Security
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
 
         var user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new RuntimeException("User not found"));
-        ;
+
+        var jwtToken = jwtService.generateToken(user.getEmail());
 
         return AuthResponse.builder()
+                .token(jwtToken)
                 .email(user.getEmail())
                 .role(user.getRole().name())
                 .build();
