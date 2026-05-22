2504308af6f09418e4f788b62cd5450e6529e9a9
Асель Абдрахманова <dellya4>
2026-05-23 01:08:04 +0500

    feat: implement JWT authentication

---
+++ src/main/java/org/example/sdf_final/security/JwtService.java
@@ -0,0 +1,43 @@
+package org.example.sdf_final.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private final String SECRET_KEY = "c2VjcmV0S2V5X0FjY2Vzcy1Ub2tlbl9TUEVDSUFMX0tFWV8xMjM0NTY3OA==";
+
+    // Generates token, it will work of 10 hours
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Finds email throw token
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // Verifies the validity of the token
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes())).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
