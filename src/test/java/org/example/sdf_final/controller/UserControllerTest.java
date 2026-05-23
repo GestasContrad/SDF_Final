d76e8b6171056d4b7b42a73b5c3af296452ae7ce
Асель Абдрахманова <dellya4>
2026-05-23 19:36:24 +0500

    fix: fix SubmissionControllerTest

--- src/test/java/org/example/sdf_final/controller/UserControllerTest.java
+++ src/test/java/org/example/sdf_final/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package org.example.sdf_final.controller;
 
 import org.example.sdf_final.dto.request.UpdateProfileRequest;
+import org.example.sdf_final.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -13,18 +14,27 @@ class UserControllerTest extends BaseControllerTest {
 
     // Get user - success
     @Test
+    @WithMockUser
     void getUser_shouldReturn200() throws Exception {
-        mockMvc.perform(get("/api/users/profile/1"))
+        User teacher = createTeacher();
+        Long generatedId = teacher.getId();
+        mockMvc.perform(get("/api/users/profile/" +  generatedId))
                 .andExpect(status().isOk());
     }
 
     // Update profile - success
     @Test
+    @WithMockUser
     void updateProfile_shouldReturn200() throws Exception {
+        User teacher = createTeacher();
+        Long generatedId = teacher.getId();
+
         UpdateProfileRequest request = new UpdateProfileRequest();
         request.setFirstName("NewName");
+        request.setLastName(teacher.getLastName());
+        request.setEmail(teacher.getEmail());
 
-        mockMvc.perform(put("/api/users/profile/1")
+        mockMvc.perform(put("/api/users/profile/" + generatedId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk());
@@ -34,6 +44,8 @@ class UserControllerTest extends BaseControllerTest {
     @Test
     @WithMockUser(roles = "ADMIN")
     void getAllUsers_shouldReturn200() throws Exception {
+        User teacher = createTeacher();
+        User student = createStudent();
 
         mockMvc.perform(get("/api/users"))
                 .andExpect(status().isOk());
@@ -43,8 +55,10 @@ class UserControllerTest extends BaseControllerTest {
     @Test
     @WithMockUser(roles = "ADMIN")
     void deleteUser_shouldReturn204() throws Exception {
+        User teacher = createTeacher();
+        Long generatedId = teacher.getId();
 
-        mockMvc.perform(delete("/api/users/1"))
+        mockMvc.perform(delete("/api/users/" + generatedId))
                 .andExpect(status().isNoContent());
     }
 }
